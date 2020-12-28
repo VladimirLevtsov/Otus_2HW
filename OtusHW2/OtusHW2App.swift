939c1e2212de47ff9120af4fd90fe10e553361f2
Vladimir Levtsov <levtsov.vladimir@gmail.com>
2020-12-28 14:27:17 +0300

    Initial Commit

---
+++ OtusHW2/OtusHW2App.swift
@@ -0,0 +1,17 @@
+//
+//  OtusHW2App.swift
+//  OtusHW2
+//
+//  Created by VLADIMIR LEVTSOV on 28.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct OtusHW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
